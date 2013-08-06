9e21bd40c92b12e6f657544abf93c75a85b2359a
prasantakumarswain <prasantakumarswain37@gmail.com>
2013-08-06 17:37:22 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BirthdayPortal::Application.config.secret_token = '77823e81d427c3fceb18501c30cd53e1ec0c41c54f68f9f4db7253a786e730de1370ce35b1d6cba2cbcc45e56495bf45485a2304ab5b802ee38b05d7d5df7957'
